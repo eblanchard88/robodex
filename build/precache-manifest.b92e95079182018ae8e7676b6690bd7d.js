598ddb0e8b324b387e5d28c042de70a81721632a
Eric Blanchard <eblanchard99@yahoo.com>
2020-04-28 22:50:41 -0400

    moving files

---
+++ build/precache-manifest.b92e95079182018ae8e7676b6690bd7d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a50a839fb65320d35f5f97627524d103",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d1b4e2f401737e1dc808",
+    "url": "./static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "c574879243527354c8e3",
+    "url": "./static/css/main.3fcc1db7.chunk.css"
+  },
+  {
+    "revision": "d1b4e2f401737e1dc808",
+    "url": "./static/js/2.9d000609.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.9d000609.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c574879243527354c8e3",
+    "url": "./static/js/main.cff1ce30.chunk.js"
+  },
+  {
+    "revision": "9ab7c2c24355dace4cd2",
+    "url": "./static/js/runtime-main.9d7bf708.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "./static/media/SEGA.12a3db8b.woff"
+  }
+]);
